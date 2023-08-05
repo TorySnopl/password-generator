15e9c29f83d945d8e87fd129b62e3aa5da59b935
TorySnopl <Paramedic064@gmail.com>
2023-08-05 12:18:28 -0700

    maximum stack error on lines 27 and 52

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,69 @@
 // Assignment code here
+const characters = {
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "0123456789",
+  symbols: "!@#$%^&*()_+~"
+};
 
+  let pLength = prompt("Please choose the length of your password between 8 and 128");
+  let pUpper = prompt('Do you want to use uppercase letters? type y for yes or n for no.');
+  let pNumber = prompt('Do you want to use numbers? type y for yes or n for no.');
+  let pSpecial = prompt('Do you want to use special characters? type y for yes and n for no.')
+  let res = confirm(`You want your password length set at ${pLength}. 
+  Will your password contain Uppercase letters? ${pUpper}. 
+  Will your password contain a number? ${pNumber}. 
+  Will your password contain a special character? ${pSpecial}`);
+
+  if (res === true) {
+    alert('generating password now');
+    generatePassword();
+   } else { 
+    alert('please try again')
+  };
+
+    function generatePassword(){
+    if (pUpper === 'y' && pNumber === 'y' && pSpecial === 'y'){
+      Math.floor(Math.random() * characters.uppercase.length);
+      Math.floor(Math.random() * characters.lowercase.length);
+      Math.floor(Math.random() * characters.numbers.length);
+      Math.floor(Math.random() * characters.symbols.length);
+    } else if (pUpper === 'y' && pNumber === 'y' && pSpecial ==='n'){
+      Math.floor(Math.random() * characters.uppercase.length);
+      Math.floor(Math.random() * characters.lowercase.length);
+      Math.floor(Math.random() * characters.numbers.length);
+    } else if (pUpper === 'y' && pNumber === 'n' && pSpecial === 'n'){
+      Math.floor(Math.random() * characters.uppercase.length);
+      Math.floor(Math.random() * characters.lowercase.length);
+    } else if (pUpper == 'n' && pNumber === 'y' && pSpecial === 'y'){
+      Math.floor(Math.random() * characters.lowercase.length);
+      Math.floor(Math.random() * characters.numbers.length);
+      Math.floor(Math.random() * characters.symbols.length);
+    } else if (pUpper === 'n' && pNumber === 'n' && pSpecial === 'y'){
+      Math.floor(Math.random() * characters.lowercase.length);
+      Math.floor(Math.random() * characters.symbols.length);
+    } else if (pUpper === 'n' && pNumber === 'n' && pSpecial === 'n') {
+      Math.floor(Math.random() * characters.lowercase.length);
+    } else if (pUpper === 'n' && pNumber === 'y' && pSpecial === 'n'){
+      Math.floor(Math.random() * characters.lowercase.length);
+      Math.floor(Math.random() * characters.numbers.length);
+    };
+
+    while (generatePassword().length<pLength);
+
+    return password;
+  
+  };
+
+
+
+
+
+     
+    
+    
+
+console.log(generatePassword())
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,4 +78,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
